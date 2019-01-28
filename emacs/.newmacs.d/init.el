4013f8506f9723bb96f2ebe48dc9efc9a7857d87
Maurn <maurn95@gmail.com>
2019-01-28 14:01:40 +0100

    emacs

---
+++ emacs/.newmacs.d/init.el
@@ -0,0 +1,449 @@
+;;; init.el --- Initialization file for Emacs  -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Emacs Startup File --- initialization for Emacs
+;;; Package --- Summary
+;;; Code:
+(eval-and-compile
+  (setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6))
+
+(with-eval-after-load 'display-line-numbers
+  (defvar display-line-numbers-type 'relative))
+
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+(setq-default indent-tabs-mode nil)
+(global-hl-line-mode 1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(blink-cursor-mode 0)
+(winner-mode 1)
+(put 'narrow-to-region 'disabled nil)
+(defvar show-paren-delay 0)
+(show-paren-mode 1)
+(electric-pair-mode 1)
+
+;; Disable backup files
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
+
+(add-to-list 'default-frame-alist '(font . "SourceCodeProSemibold-11"))
+
+(defun my-prog-mode-hook ()
+  "Prog hook!"
+  (flycheck-mode)
+  (display-line-numbers-mode))
+
+(add-hook 'prog-mode-hook 'my-prog-mode-hook)
+
+;;;We’re going to set the load-path ourselves and avoid calling (package-initilize) (for performance reasons) so we need to set package--init-file-ensured to true to tell package.el to not automatically call it on our behalf. Additionally we’re setting package-enable-at-startup to nil so that packages will not automatically be loaded for us since use-package will be handling that.
+(eval-and-compile
+  (setq load-prefer-newer t
+        package-user-dir "~/.newmacs.d/elpa"
+        package--init-file-ensured t
+        package-enable-at-startup nil)
+
+  (unless (file-directory-p package-user-dir)
+    (make-directory package-user-dir t))
+
+  (setq load-path (append load-path (directory-files package-user-dir t "^[^.]" t))))
+
+
+(eval-when-compile
+  (require 'package)
+  ;; tells emacs not to load any packages before starting up
+  ;; the following lines tell emacs where on the internet to look up
+  ;; for new packages.
+  (setq package-archives '(("melpa"     . "https://melpa.org/packages/")
+                           ("elpa"      . "https://elpa.gnu.org/packages/")
+                           ("org"  . "https://orgmode.org/elpa/")))
+  ;; (package-initialize)
+  (unless package--initialized (package-initialize t))
+
+  ;; Bootstrap `use-package'
+  (unless (package-installed-p 'use-package) ; unless it is already installed
+    (package-refresh-contents) ; update packages archive
+    (package-install 'use-package)) ; and install the most recent version of use-package
+
+  (require 'use-package)
+  (setq use-package-always-ensure t))
+
+(use-package which-key
+  :config
+  (which-key-mode 1)
+  (setq which-key-idle-delay 0.1))
+
+(use-package general
+  :after which-key
+  :config
+  (general-override-mode 1)
+
+  (defun find-user-init-file ()
+    "Edit the `user-init-file', in same window."
+    (interactive)
+    (find-file user-init-file))
+  (defun load-user-init-file ()
+    "Load the `user-init-file', in same window."
+    (interactive)
+    (load-file user-init-file))
+
+    (defun last-buffer (&optional buffer visible-ok frame)
+    "Return the last buffer in FRAME's buffer list.
+    If BUFFER is the last buffer, return the preceding buffer
+    instead.  Buffers not visible in windows are preferred to visible
+    buffers, unless optional argument VISIBLE-OK is non-nil.
+    Optional third argument FRAME nil or omitted means use the
+    selected frame's buffer list.  If no such buffer exists, return
+    the buffer `*scratch*', creating it if necessary."
+    (setq frame (or frame (selected-frame)))
+    (or (get-next-valid-buffer (nreverse (buffer-list frame))
+                                buffer visible-ok frame)
+        (get-buffer "*scratch*")
+        (let ((scratch (get-buffer-create "*scratch*")))
+            (set-buffer-major-mode scratch)
+            scratch)))
+
+
+  ;;Taken from http://emacsredux.com/blog/2013/05/04/rename-file-and-buffer/
+  ;; (defun rename-file-and-buffer ()
+  ;;   "Rename the current buffer and file it is visiting."
+  ;;   (interactive)
+  ;;   (let ((filename (buffer-file-name)))
+  ;;     (if (not (and filename (file-exists-p filename)))
+  ;;         (message "Buffer is not visiting a file!")
+  ;;       (let ((new-name (read-file-name "New name: " filename)))
+  ;;         (cond
+  ;;          ((vc-backend filename) (vc-rename-file filename new-name))
+  ;;          (t
+  ;;           (rename-file filename new-name t)
+  ;;           (set-visited-file-name new-name t t)))))))
+(defun alternate-buffer (&optional window)
+  "Switch back and forth between current and last buffer in the current WINDOW."
+  (interactive)
+  (let ((current-buffer (window-buffer window)))
+    ;; if no window is found in the windows history, `switch-to-buffer' will
+    ;; default to calling `other-buffer'.
+    (switch-to-buffer
+     (cl-find-if (lambda (buffer)
+                   (not (eq buffer current-buffer)))
+                 (mapcar #'car (window-prev-buffers window)))
+     nil t)))
+
+(defun alternate-window ()
+  "Switch back and forth between current and last window in the current frame."
+  (interactive)
+  (let (;; switch to first window previously shown in this frame
+        (prev-window (get-mru-window nil t t)))
+    ;; Check window was not found successfully
+    (unless prev-window (user-error "Last window not found"))
+    (select-window prev-window)))
+
+  (general-create-definer tyrant-def
+    :states '(normal visual insert motion emacs)
+    :prefix "SPC"
+    :non-normal-prefix "C-SPC")
+
+  (general-create-definer despot-def
+    :states '(normal insert)
+    :prefix "SPC"
+    :non-normal-prefix "C-SPC")
+
+  (general-define-key
+    :keymaps 'key-translation-map
+    "ESC" (kbd "C-g"))
+
+  (general-def
+    "C-x x" 'eval-defun)
+
+  (tyrant-def
+    ""     nil
+    "c"   (general-simulate-key "C-c")
+    "h"   (general-simulate-key "C-h")
+    "u"   (general-simulate-key "C-u")
+    "x"   (general-simulate-key "C-x")
+    "TAB" 'alternate-buffer
+    "/"   'counsel-rg
+
+    ;; Quit operations
+    "q"	  '(:ignore t :which-key "quit")
+    "qq"  'kill-emacs
+    "qz"  'delete-frame
+
+    ;; Buffer operations
+    "b"   '(:ignore t :which-key "buffers")
+    "bd"  'kill-this-buffer
+    "bn"  'next-buffer
+    "bp"  'previous-buffer
+    "bq"  'kill-buffer-and-window
+    "bR"  'rename-file-and-buffer
+    "br"  'revert-buffer
+
+    ;; Window operations
+    "w"   '(:ignore t :which-key "windows")
+    "w TAB" 'alternate-window
+    "wm"  'maximize-window
+    "wv"  'split-window-horizontally
+    "ws"  'split-window-vertically
+    "wV"  (lambda () (interactive)(split-window-horizontally) (other-window 1))
+    "wS"  (lambda () (interactive)(split-window-vertically) (other-window 1))
+    "wm"  'maximize-window
+    "wu"  'winner-undo
+    "ww"  'other-window
+    "wd"  'delete-window
+    "wD"  'delete-other-windows
+
+    ;; File operations
+    "f"   '(:ignore t :which-key "files")
+    "fc"  'write-file
+    "fe"  '(:ignore t :which-key "emacs")
+    "fed" 'find-user-init-file
+    "feR" 'load-user-init-file
+    "fj"  'dired-jump
+    "fl"  'find-file-literally
+    "fR"  'rename-file-and-buffer
+    "fs"  'save-buffer
+
+    ;; Applications
+    "a"   '(:ignore t :which-key "Applications")
+    "ad"  'dired
+    ":"   'shell-command
+    ";"   'eval-expression
+    "ac"  'calendar
+    "oa"  'org-agenda)
+
+  (general-def 'normal doc-view-mode-map
+    "j"   'doc-view-next-line-or-next-page
+    "k"   'doc-view-previous-line-or-previous-page
+    "gg"  'doc-view-first-page
+    "G"   'doc-view-last-page
+    "C-d" 'doc-view-scroll-up-or-next-page
+    "C-f" 'doc-view-scroll-up-or-next-page
+    "C-b" 'doc-view-scroll-down-or-previous-page)
+
+  (general-def '(normal visual) outline-minor-mode-map
+    "zn"  'outline-next-visible-heading
+    "zp"  'outline-previous-visible-heading
+    "zf"  'outline-forward-same-level
+    "zB"  'outline-backward-same-level)
+
+  (general-def 'normal package-menu-mode-map
+    "i"   'package-menu-mark-install
+    "U"   'package-menu-mark-upgrades
+    "d"   'package-menu-mark-delete
+    "u"   'package-menu-mark-unmark
+    "x"   'package-menu-execute
+    "q"   'quit-window))
+
+(use-package doom-themes
+  :config
+  (load-theme 'doom-one t))
+
+(use-package doom-modeline
+  :hook (after-init . doom-modeline-mode))
+
+(use-package evil
+  :hook (after-init . evil-mode)
+  :config
+  (evil-set-initial-state 'shell-mode 'normal)
+  (evil-set-initial-state 'doc-view-mode 'normal)
+  (evil-set-initial-state 'package-menu-mode 'normal)
+  (evil-set-initial-state 'biblio-selection-mode 'motion)
+  (defvar doc-view-continuous t)
+  :general
+  (tyrant-def
+    "wh"  'evil-window-left
+    "wl"  'evil-window-right
+    "wj"  'evil-window-down
+    "wk"  'evil-window-up
+    "bN"  'evil-buffer-new
+    "fd"  'evil-save-and-close))
+
+(use-package evil-surround
+  :after evil
+  :config (global-evil-surround-mode 1))
+
+(use-package evil-commentary
+  :after evil
+  :config (evil-commentary-mode 1)
+  :general
+  ('normal override-global-map
+    "gc"  'evil-commentary
+    "gC" 'evil-commentary-line))
+
+(use-package evil-visualstar
+  :after evil
+  :config
+  (defvar evilmi-always-simple-jump t)
+  (global-evil-visualstar-mode 1))
+
+(use-package company
+  :hook (after-init . global-company-mode)
+  :config
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-j") #'company-select-next-or-abort)
+  (define-key company-active-map (kbd "C-k") #'company-select-previous-or-abort)
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1)
+  (defvar company-dabbrev-ignore-case t)
+  (setq company-frontends '(company-echo-metadata-frontend
+                            company-pseudo-tooltip-unless-just-one-frontend
+                            company-preview-frontend))
+  (setq company-backends '((company-capf
+                            company-files)
+                           (company-dabbrev-code company-keywords)
+                            company-dabbrev company-yasnippet)))
+
+(use-package ivy
+    :hook (after-init . ivy-mode)
+    :config (setq ivy-use-virtual-buffers t
+                ivy-count-format "(%d/%d) "
+                ivy-initial-inputs-alist nil
+                ivy-re-builders-alist '((t . ivy--regex-ignore-order)))
+                (define-key ivy-minibuffer-map (kbd "C-j") 'ivy-next-line)
+                (define-key ivy-minibuffer-map (kbd "C-k") 'ivy-previous-line)
+
+    :commands (ivy-switch-buffer)
+    :general
+    (tyrant-def "bb"  'ivy-switch-buffer))
+
+(use-package smex)
+
+(use-package counsel
+  :after (ivy)
+  :general
+  (tyrant-def
+    "SPC" 'counsel-M-x
+    "ff"  'counsel-find-file
+    "fr"  'counsel-recentf
+    "fL"  'counsel-locate))
+
+(use-package projectile)
+
+(use-package counsel-projectile
+  :after (projectile ivy)
+  :general
+  (tyrant-def
+   "p"   '(:ignore t :which-key "projectile")
+   "pd"  'counsel-projectile-dired-find-dir
+   "po"  'counsel-projectile-find-other-file
+   "pf"  'counsel-projectile-find-file
+   "pp"  'counsel-projectile-switch-project
+   "pb"  'counsel-projectile-switch-to-buffer
+   "bp"  'counsel-projectile-switch-to-buffer))
+
+
+(use-package flycheck
+  :commands (flycheck-mode)
+  :general
+  (tyrant-def
+   "e"   '(:ignore t :which-key "errors")
+   "en"  'flycheck-next-error
+   "ep"  'flycheck-previous-error))
+
+(use-package magit
+  :commands (magit-status)
+  :general
+  (tyrant-def
+   "g"   '(:ignore t :which-key "git")
+   "gs"  'magit-status))
+
+(use-package evil-magit
+  :hook (magit-mode . evil-magit-init))
+
+(use-package treemacs
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs              (if (executable-find "python") 3 0)
+          treemacs-deferred-git-apply-delay   0.5
+          treemacs-display-in-side-window     t
+          treemacs-file-event-delay           5000
+          treemacs-file-follow-delay          0.2
+          treemacs-follow-after-init          t
+          treemacs-follow-recenter-distance   0.1
+          treemacs-git-command-pipe           ""
+          treemacs-goto-tag-strategy          'refetch-index
+          treemacs-indentation                2
+          treemacs-indentation-string         " "
+          treemacs-is-never-other-window      nil
+          treemacs-max-git-entries            5000
+          treemacs-no-png-images              nil
+          treemacs-no-delete-other-windows    t
+          treemacs-project-follow-cleanup     nil
+          treemacs-persist-file               (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-recenter-after-file-follow nil
+          treemacs-recenter-after-tag-follow  nil
+          treemacs-show-cursor                nil
+          treemacs-show-hidden-files          t
+          treemacs-silent-filewatch           nil
+          treemacs-silent-refresh             nil
+          treemacs-sorting                    'alphabetic-desc
+          treemacs-space-between-root-nodes   t
+          treemacs-tag-follow-cleanup         t
+          treemacs-tag-follow-delay           1.5
+          treemacs-width                      35)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode t)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null (executable-find "python3"))))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-evil
+  :after treemacs evil
+  :ensure t)
+
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :after treemacs dired
+  :ensure t
+  :config (treemacs-icons-dired-mode))
+
+(use-package typescript-mode
+  :mode (("\\.ts\\'" . typescript-mode)))
+
+(use-package tide
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save))
+  :config ((setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (company-mode +1)
+    (setq company-tooltip-align-annotations t)
+    (setq tide-completion-ignore-case t)))
+
+(eval-when-compile
+(setq-default custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+(load custom-file)))
+
+(eval-and-compile
+(add-hook 'emacs-startup-hook '(lambda ()
+                (setq gc-cons-threshold 16777216
+                        gc-cons-percentage 0.1))))
+(provide 'init)
+;;; init.el ends here
