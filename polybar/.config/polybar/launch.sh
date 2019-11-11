1a1be02135bbe9342a10004207aa2561d78092dc
Maurn <maurn95@gmail.com>
2019-11-11 20:18:36 +0100

    haha

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,8 +9,8 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 hostname=$(hostname)
 if [ $hostname == "spanden" ]; then
     # Launch bar1 and bar2
-    MONITOR=DVI-D-0 polybar -r bottom &
-    MONITOR=DVI-I-1 polybar -r bottom &
+    MONITOR=HDMI-2 polybar -r bottom &
+    MONITOR=DVI-D-1 polybar -r bottom &
 else
     MONITOR=eDP-1 polybar -r bottom &
 fi
