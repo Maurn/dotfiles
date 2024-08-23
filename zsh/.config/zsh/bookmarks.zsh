7246bf57b4cf8d53931d2021ff37b4b4852b5a33
maurn <maurn95@gmail.com>
2024-08-23 17:14:24 +0200

    bm

--- zsh/.config/zsh/bookmarks.zsh
+++ zsh/.config/zsh/bookmarks.zsh
@@ -2,9 +2,7 @@ bm () {
     hash -d $1=$2
 }
 
-bm ysi /home/maurn/entropyfox/YSI
-bm nginx /etc/nginx
-bm sites-available /etc/nginx/sites-available
-bm sites-enabled /etc/nginx/sites-enabled
+bm mono /home/maurn/code/monorepo
+bm infra /home/maurn/code/infrastructure
 
 unfunction bm
