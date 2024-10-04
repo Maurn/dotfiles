9d67a99e9736002b7626a6d3a135eeb7f4a86bbf
maurn <maurn95@gmail.com>
2024-10-04 11:45:08 +0200

    stuff

--- emacs/.emacs.d/editor.el
+++ emacs/.emacs.d/editor.el
@@ -252,14 +252,6 @@
         xref-show-definitions-function #'consult-xref)
 
   :config
-  (consult-customize
-   consult-theme :preview-key '(:debounce 0.2 any)
-   consult-ripgrep consult-git-grep consult-grep
-   consult-bookmark consult-recent-file consult-xref
-   consult--source-bookmark consult--source-file-register
-   consult--source-recent-file consult--source-project-recent-file
-   ;; :preview-key "M-."
-   :preview-key '(:debounce 0.4 any))
   (setq consult-narrow-key "<")
 
   :general
